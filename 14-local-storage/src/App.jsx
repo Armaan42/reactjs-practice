d88c751cb5831e5edee60c800261a6f5578487f7
Armaan <armaansinghbhau8@gmail.com>
2026-06-09 01:17:44 +0530

    learn about the local storage concepts

--- 14-local-storage/src/App.jsx
+++ 14-local-storage/src/App.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 const App = () => {
 
+  // localStorage.setItem('user','armaan')
+
+  // const user = localStorage.getItem('user')
+  // console.log(user); 
+
+  const users = {
+    username: 'armaan',
+    age: 21,
+    gender: 'male'
+  }  
+
+  // localStorage.setItem('user', JSON.stringify(users));
+
+  const user = JSON.parse(localStorage.getItem('user'));
+  console.log(user);
   
 
   return (
