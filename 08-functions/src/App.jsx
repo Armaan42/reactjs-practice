bcc6f67a03180c4cb0e20f66659808a2b9391bc1
Armaan <armaansinghbhau8@gmail.com>
2026-06-08 15:17:04 +0530

    learn about the functions

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => {
+
+  const pageScrolling = (value) => {
+    // console.log('page scrolling at speed', value);
+
+    if(value > 0) {
+      console.log('seedha scrolling');
+    }
+    else{
+      console.log('ulta scrolling');
+    }
+  }
+
+  return (
+    <div onWheel={(elem) => {
+      // console.log(elem.deltaY);
+      pageScrolling(elem.deltaY)
+    }} >
+      <div className="page1"></div>
+      <div className="page2"></div>
+      <div className="page3"></div>
+    </div>
+  )
+}
+
+export default App
