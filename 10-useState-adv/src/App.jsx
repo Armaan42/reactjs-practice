ceaff5834e80302df26e5b74794daa4b700719ab
Armaan <armaansinghbhau8@gmail.com>
2026-06-08 16:29:24 +0530

    update state management to use an object with user and age properties

--- 10-useState-adv/src/App.jsx
+++ 10-useState-adv/src/App.jsx
@@ -2,14 +2,18 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [num, setNum] = useState(10)
+  const [num, setNum] = useState({user:'aaa', age:12})
 
   return (
     <div>
-      <h1>{num}</h1>
+      <h1>{num.user}, {num.age}</h1>
       <button onClick={() => {
-        setNum(20);
-        console.log(num);
+
+        const newNum = {...num}
+        newNum.user = 'bbb'
+        newNum.age = 15
+        setNum(newNum)
+
       }} >click</button>
     </div>
   )
