876fd3158b92ae796bb553245115a23c0530f253
Armaan <armaansinghbhau8@gmail.com>
2026-06-08 15:17:45 +0530

    useState hook

---
+++ 09-useState/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <button>Increase</button>
+      <h1></h1>
+      <button>Decrease</button>
+    </div>
+  )
+}
+
+export default App
