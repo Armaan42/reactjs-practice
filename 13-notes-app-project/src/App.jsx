abf92a3b366ffc214892842282a1daf32d3bfc6a
Armaan <armaansinghbhau8@gmail.com>
2026-06-08 20:30:21 +0530

    Add Tailwind CSS integration and form handling in App component

--- 13-notes-app-project/src/App.jsx
+++ 13-notes-app-project/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const App = () => {
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log('Form Submitted');
+  }
+
   return (
-    <div>
-      App
+    <div className='h-screen bg-gray-950 text-white'>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }} className='flex justify-between items-start p-10 gap-5 flex-col'>
+          <input className='w-full font-medium px-5 py-2 border-3 rounded outline-none ' type="text" placeholder='Enter Notes heading' />
+          <textarea className='w-full px-5 py-2 border-2 rounded h-40 font-medium outline-none' type="text" placeholder='Write details'/>
+          <button className='bg-white font-medium text-black px-5 py-2 rounded outline-none' >Add Notes</button>
+      </form>
     </div>
   )
 }
 
-export default App
+export default App 
