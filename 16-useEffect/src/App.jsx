1cf7e43e46868b8a1df345de1766cd60cb630c81
Armaan <armaansinghbhau8@gmail.com>
2026-06-10 00:21:18 +0530

    learn about the useEffect and useState

--- 16-useEffect/src/App.jsx
+++ 16-useEffect/src/App.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const App = () => {
 
-  const [num, setNum] = useState(0)
-  const [num2, setNum2] = useState(100)
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
+
+  function aChanging() {
+    console.log('A changed');
+  }
+
+  function bChanging() {
+    console.log('B changed');
+  }
 
   useEffect(function() {
-    console.log('useffect is running');
-  },[num])
-  
+    aChanging()
+    console.log('useEffect is running...')
+  },[a])
+
   return (
     <div>
-      <h1>num {num}</h1>
-      <h1>num2 {num2} </h1>
-      <button onMouseEnter={() => {
-        setNum(num+1)
-      }} onMouseLeave={() => {
-        setNum2(num2+1)
-      }} >Click</button>
+      <h1>A  is {a}</h1>
+      <h1>B  is {b}</h1>
+
+      <button onClick={()=>{
+        setA(a+1)
+      }} >Change A</button>
+
+      <button onClick={()=>{
+        setB(b+1)
+      }} >Change B</button>
     </div>
   )
 }
