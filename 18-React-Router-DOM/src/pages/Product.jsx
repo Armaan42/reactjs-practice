6816e42452447af99092fc74d94d96171370c2f5
Armaan <armaansinghbhau8@gmail.com>
2026-06-11 00:31:39 +0530

    bug fix: Pages were overlapping

--- 18-React-Router-DOM/src/pages/Product.jsx
+++ 18-React-Router-DOM/src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 const Product = () => {
   return (
@@ -7,8 +7,10 @@ const Product = () => {
         <div className='flex justify-center gap-10 p-10' >
             <Link className='text-lg font-medium' to='/Product/men' >Men</Link>
             <Link className='text-lg font-medium' to='/Product/women' >Women</Link>
+            <Link className='text-lg font-medium' to='/Product/kids' >Kids</Link>
         </div> 
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-5xl font-medium underline underline-offset-8" >Product</h1>
+      {/* <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-5xl font-medium underline underline-offset-8" >Product</h1> */}
+      <Outlet/>
     </div>
   )
 }
