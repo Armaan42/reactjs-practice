e80f429925414c43794efd1f9823f184f3f37784
Armaan <armaansinghbhau8@gmail.com>
2026-06-11 00:32:03 +0530

    Added the inner routes

--- 18-React-Router-DOM/src/App.jsx
+++ 18-React-Router-DOM/src/App.jsx
@@ -8,6 +8,7 @@ import Product from './pages/Product'
 import Men from './pages/Men'
 import Women from './pages/Women'
 import NotFound from './pages/NotFound'
+import Kids from './pages/Kids'
 
 const App = () => {
   return (
@@ -19,10 +20,12 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} /> 
-        <Route path='/Product' element={<Product />} />   
 
-        <Route path='/Product/men' element={<Men />} />
-        <Route path='/Product/women' element={<Women />} />
+        <Route path='/Product' element={<Product />}>
+          <Route path='men' element={<Men />} />
+          <Route path='women' element={<Women />} />
+          <Route path='kids' element={<Kids />} />
+        </Route>   
 
         <Route path='*' element={<NotFound />} />
 
