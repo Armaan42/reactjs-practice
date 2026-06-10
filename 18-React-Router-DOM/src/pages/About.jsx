035d76ff25cb230c749b586dcf02eff968a20af7
Armaan <armaansinghbhau8@gmail.com>
2026-06-11 02:25:21 +0530

    Added useNavigate() and also learned about naivgate to return to previous page

--- 18-React-Router-DOM/src/pages/About.jsx
+++ 18-React-Router-DOM/src/pages/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+
+    const navigate = useNavigate()
+
   return (
     <div>
+        <button onClick={()=>{
+            navigate('/')
+        }} className='bg-green-300 px-5 py-2 m-5 rounded cursor-pointer active:scale-95' >Return to Home Page</button>
+
+        <button onClick={()=>{
+            navigate(-1)
+        }} className='bg-green-300 px-5 py-2 m-5 rounded cursor-pointer active:scale-95' >Back</button>
+
       <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-5xl font-medium underline underline-offset-8" >About</h1>
     </div>
   )
