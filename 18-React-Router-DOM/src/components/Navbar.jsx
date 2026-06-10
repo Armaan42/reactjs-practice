567555945175946af9b63f1239d562b31a041e02
Armaan <armaansinghbhau8@gmail.com>
2026-06-11 00:05:49 +0530

    Added navbar components

---
+++ 18-React-Router-DOM/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='flex items-center justify-between p-5 bg-[#1e1e1e]'>
+        <h1 className='text-xl font-bold text-white'>Logo</h1>
+        <div className='flex items-center gap-10 text-white' >
+            <Link to='/' >Home</Link>
+            <Link to='/Contact' >Contact</Link>
+            <Link to='/About' >About</Link>
+            <Link to='/Product' >Product</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
+ 
