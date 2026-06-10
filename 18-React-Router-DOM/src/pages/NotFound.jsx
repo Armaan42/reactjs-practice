0d552e28bba424d9f9c6d185f5af11d416c64d90
Armaan <armaansinghbhau8@gmail.com>
2026-06-11 00:11:10 +0530

    Added the not found page

---
+++ 18-React-Router-DOM/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-5xl font-medium underline underline-offset-8" >404 | Page not found</h1>
+    </div>
+  )
+}
+
+export default NotFound
