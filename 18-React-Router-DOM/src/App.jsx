ccb64da4dacf7e38ede69a57a435d8c501d85d26
Armaan <armaansinghbhau8@gmail.com>
2026-06-11 00:11:44 +0530

    Added the routes to every page

--- 18-React-Router-DOM/src/App.jsx
+++ 18-React-Router-DOM/src/App.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import {Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
+import Product from './pages/Product'
+import Men from './pages/Men'
+import Women from './pages/Women'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
     <div>
+
+      <Navbar />
+
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} /> 
+        <Route path='/Product' element={<Product />} />   
+
+        <Route path='/Product/men' element={<Men />} />
+        <Route path='/Product/women' element={<Women />} />
+
+        <Route path='*' element={<NotFound />} />
+
       </Routes>
+
     </div>
   )
 }
